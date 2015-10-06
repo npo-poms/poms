00a903659bdddb1b8f527008d8ce41f1e57b2f03
Tom Kruijsen <tom@tomkr.org>
2015-10-06 15:08:28 +0200

    Add specs for fetch_current_broadcast_and_key

--- spec/integration/poms_spec.rb
+++ spec/integration/poms_spec.rb
@@ -17,4 +17,16 @@ describe Poms do
       expect(current_broadcast.schedule_events.last.starts_at).to be_present
     end
   end
+
+  describe '#fetch_current_broadcast' do
+    let(:current_broadcast) { Poms.fetch_current_broadcast_and_key('NED3') }
+
+    it 'has a key' do
+      expect(current_broadcast[:key]).to be_present
+    end
+
+    it 'has a broadcast with a mid' do
+      expect(current_broadcast[:broadcast].mid).to be_present
+    end
+  end
 end
