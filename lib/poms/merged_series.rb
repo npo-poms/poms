554cbcbfb957f01674161182960ef0e3c3486b00
Tom Kruijsen <tom@tomkruijsen.nl>
2016-01-05 17:56:31 +0100

    Add some more comments.

--- lib/poms/merged_series.rb
+++ lib/poms/merged_series.rb
@@ -8,11 +8,17 @@ module Poms
 
     API_URL = 'https://rs-test.poms.omroep.nl/v1/api/media/redirects/'.freeze
 
+    # Gets the merged serie mids as a hash. Expects a JSON response from
+    # the server with a `map` key.
+    #
+    # @param api_url the API url to query
+    # @return [Hash] a hash with old_mid => new_mid pairs
     def serie_mids(api_url)
       data = open(api_url).read
       JSON.parse(data).fetch('map')
     end
 
+    # Loops over the serie mids of the default api url.
     def each
       serie_mids(API_URL).each do |old_mid, new_mid|
         yield(old_mid, new_mid)
