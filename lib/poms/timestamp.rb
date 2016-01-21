ad8303bf4ecf1e3fc0e291d34f38579023e91050
Tom Kruijsen <tom@tomkruijsen.nl>
2016-01-21 13:07:06 +0100

    Rubocop.

--- lib/poms/timestamp.rb
+++ lib/poms/timestamp.rb
@@ -2,7 +2,7 @@ module Poms
   # Functionality for manipulating Poms timestamps
   module Timestamp
     module_function
-    
+
     def convert(poms_timestamp)
       Time.at(poms_timestamp / 1000).to_datetime
     end
--- spec/lib/poms/timestamp_spec.rb
+++ spec/lib/poms/timestamp_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Poms::Timestamp do
   it 'converts Poms timestamps to DateTime' do
-    expect(Poms::Timestamp.convert(1453217700000))
+    expect(Poms::Timestamp.convert(1_453_217_700_000))
       .to eq(Time.parse '2016-01-19 16:35:00 +0100')
   end
 end
