f9ecafea6deef5cf64455ac8ae50b83c55cb2d89
Tom Kruijsen <tom@tomkruijsen.nl>
2016-02-26 17:30:52 +0100

    Merge into default options instead of the other way around

--- lib/poms/views.rb
+++ lib/poms/views.rb
@@ -16,30 +16,26 @@ module Poms
     end
 
     def by_group(mid)
-      args = {
-        key: "\"#{mid}\""
-      }.merge(DEFAULT_OPTIONS)
+      args = DEFAULT_OPTIONS.merge(key: "\"#{mid}\"")
       construct_view_url('by-group', args)
     end
 
     # Constructs a url using the by-ancestor-and-type view of Poms.
     def descendants_by_type(mid, type = 'BROADCAST', options = {})
-      options = DEFAULT_OPTIONS.merge(options)
-      args = {
-        key: "[\"#{mid}\", \"#{type}\"]"
-      }.merge(options)
+      args = DEFAULT_OPTIONS.merge(key: "[\"#{mid}\", \"#{type}\"]")
+             .merge(options)
       construct_view_url('by-ancestor-and-type', args)
     end
 
     def broadcasts_by_channel_and_start(channel, start_time = Time.now,
                                         end_time = 1.day.ago, limit = 1,
                                         descending = true)
-      args = {
+      args = DEFAULT_OPTIONS.merge(
         startkey: "[\"#{channel}\", #{to_poms_timestamp(start_time)}]",
         endkey: "[\"#{channel}\", #{to_poms_timestamp(end_time)}]",
         limit: limit,
         descending: descending
-      }.merge(DEFAULT_OPTIONS)
+      )
       construct_view_url('broadcasts-by-channel-and-start', args)
     end
 
