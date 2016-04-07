1709e303f30d9f9eeb111a9b17a6ce87896891a7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-04-07 18:49:40 +0200

    Use Poms::URIs in media and add method for multiple mids

--- lib/poms/media.rb
+++ lib/poms/media.rb
@@ -1,9 +1,14 @@
 require 'poms/api/request'
+require 'poms/uris'
 
 module Poms
   module Media
     def self.from_mid(mid, key, secret, origin)
-      Poms::Api::Request.new("https://rs.poms.omroep.nl/v1/api/media/" + mid, key, secret, origin)
+      Poms::Api::Request.new(Poms::URIs::Media.single(mid), key, secret, origin)
+    end
+
+    def self.multiple(mids, key, secret, origin)
+      Poms::Api::Request.new(Poms::URIs::Media.multiple(mids), key, secret, origin)
     end
   end
 end
