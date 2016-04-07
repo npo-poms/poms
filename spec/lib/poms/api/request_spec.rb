6e337cbc533897d439142d20b5a2ab2315ee2798
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-04-07 18:39:58 +0200

    Improve request spec
    Don't like the fact that the class under test was being stubbed (headers
    method). There's also a bit of logic necessary for authentication in that
    private method that was not under test.

--- spec/lib/poms/api/request_spec.rb
+++ spec/lib/poms/api/request_spec.rb
@@ -1,10 +1,10 @@
-require 'poms/api/request'
 require 'spec_helper'
+require 'pry'
 
 RSpec.describe Poms::Api::Request do
   let(:subject) do
     described_class.new(
-      'https://rs.poms.omroep.nl/v1/api/media/redirects/',
+      Addressable::URI.parse('https://rs.poms.omroep.nl/v1/api/media/redirects/'),
       'key',
       'secret',
       'http://zapp.nl')
@@ -18,13 +18,20 @@ RSpec.describe Poms::Api::Request do
     end
 
     it 'sets the headers' do
-      headers = { 'Test' => 'Header' }
-      stub = stub_request(:get,
-                          'https://rs.poms.omroep.nl/v1/api/media/redirects/')
-             .with(headers: headers)
-      allow(subject).to receive(:headers).and_return(headers)
+      allow(Poms::Api::Auth).to receive(:encode).and_return('encoded-string')
+      request = a_request(
+        :get,
+        'https://rs.poms.omroep.nl/v1/api/media/redirects/'
+      ).with(
+        headers: {
+          'Origin' => 'http://zapp.nl',
+          'X-NPO-Date' => Time.now.rfc822,
+          'Authorization' => 'NPO key:encoded-string',
+          'accept' => 'application/json'
+        }
+      )
       subject.call
-      expect(stub).to have_been_requested
+      expect(request).to have_been_requested
     end
   end
 end
