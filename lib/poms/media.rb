7286ebb03e93154aa77325c8eb500e4381b3ca46
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-04-11 12:01:12 +0200

    Use PostRequest and GetRequest objects for Poms::Media endpoints

--- lib/poms/media.rb
+++ lib/poms/media.rb
@@ -4,11 +4,11 @@ require 'poms/uris'
 module Poms
   module Media
     def self.from_mid(mid, credentials)
-      Poms::Api::Request.new(Poms::URIs::Media.single(mid), credentials)
+      Poms::Api::GetRequest.new(Poms::URIs::Media.single(mid), credentials)
     end
 
     def self.multiple(mids, credentials)
-      Poms::Api::Request.new(Poms::URIs::Media.multiple(mids), credentials)
+      Poms::Api::PostRequest.new(Poms::URIs::Media.multiple, credentials, body: mids)
     end
   end
 end
