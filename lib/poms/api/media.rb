741977619c9db048936df49a1b1d3dac0907625f
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-04-11 17:44:53 +0200

    Add Poms::Api::Media.descendants and specs for Media Module

--- lib/poms/api/media.rb
+++ lib/poms/api/media.rb
@@ -5,15 +5,15 @@ module Poms
   module Api
     module Media
       def self.from_mid(mid, credentials)
-        Poms::Api::GetRequest.new(URIs::Media.single(mid), credentials)
+        GetRequest.new(URIs::Media.single(mid), credentials)
       end
 
       def self.multiple(mids, credentials)
-        Poms::Api::PostRequest.new(URIs::Media.multiple, credentials, body: mids)
+        PostRequest.new(URIs::Media.multiple, credentials, body: mids)
       end
 
-      def self.descendants
-        Poms::Api::PostRequest.new(URIs::Media.descendants, credentials, body: 'foo')
+      def self.descendants(mid, credentials, search_query)
+        PostRequest.new(URIs::Media.descendants(mid), credentials, body: search_query)
       end
     end
   end
