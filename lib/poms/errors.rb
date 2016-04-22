27f6a20131ad198c1decd5bd4bdae7310b949269
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-04-22 11:28:52 +0200

    Introduce base exception class

--- lib/poms/errors.rb
+++ lib/poms/errors.rb
@@ -1,10 +1,14 @@
 module Poms
   module Errors
+    # Generic Poms error that all other errors inherit from. This allows you to
+    # rescue any exception from this library in your application code.
+    PomsError = Class.new(StandardError)
+
     # Custom Error class to signal missing authorization attributes
-    AuthenticationError = Class.new(StandardError)
+    AuthenticationError = Class.new(PomsError)
 
     # Base exception for all Poms-specific HTTP errors.
-    HttpError = Class.new(StandardError)
+    HttpError = Class.new(PomsError)
 
     # Wrapper exception for dealing with driver-agnostic 404 responses.
     HttpMissingError = Class.new(HttpError)
@@ -14,9 +18,9 @@ module Poms
 
     # Custom error than can be used to indicate a required configuration value
     # is missing.
-    MissingConfigurationError = Class.new(StandardError)
+    MissingConfigurationError = Class.new(PomsError)
 
     # Custom error to indicate that the gem has not been configured at all.
-    NotConfigured = Class.new(StandardError)
+    NotConfigured = Class.new(PomsError)
   end
 end
