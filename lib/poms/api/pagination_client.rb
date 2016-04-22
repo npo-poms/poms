70ae46785a7b56301f45a7fe50756ccf5bf8aba0
Richard Baptist <(none)>
2016-04-22 13:44:31 +0200

    Refactor the PaginationClient

--- lib/poms/api/pagination_client.rb
+++ lib/poms/api/pagination_client.rb
@@ -1,30 +1,56 @@
 require 'poms/api/json_client'
-# require 'pry'
 
 module Poms
   module Api
-    # This module creates lazy Enumerators to handle pagination of the Poms API
-    # and performs the request on demand.
+    # Creates lazy Enumerators to handle pagination of the Poms API and performs
+    # the request on demand.
     module PaginationClient
       module_function
 
       def all(uri, config)
-        # uri.query_values = { max: 100 }
-        index = 0
         Enumerator.new do |yielder|
+          page = Page.new(uri, config)
           loop do
-            uri.query_values = { offset: index }
-            p uri
-            response = Api::JsonClient.get(uri, config)
-            total = response['total']
-            offset = response['offset']
-            amount = response['max']
-            response['items'].each do |item|
-              yielder.yield item
-            end
-            raise StopIteration if offset+amount >= total
-            index = amount+index
+            page.items.each { |item| yielder << item }
+            raise StopIteration if page.final?
+            page = page.next_page
           end
+        end
+      end
+
+      # Keep track of number of items and how many have been retrieved
+      class Page
+        def initialize(uri, config, offset = 0)
+          @uri = uri
+          @config = config
+          @offset = offset
+        end
+
+        def next_page
+          self.class.new(uri, config, next_index)
+        end
+
+        def final?
+          next_index >= response['total']
+        end
+
+        def items
+          response['items']
+        end
+
+        private
+
+        attr_reader :config, :offset, :uri
+
+        def response
+          @response ||= begin
+            uri.query_values = { offset: offset }
+            Api::JsonClient.get(uri, config)
+          end
+        end
+
+        def next_index
+          response['offset'] + response['max']
         end
       end
     end
