8a427206e22f704508ee8e267569d79990414194
Richard Baptist <rpbaptist@gmail.com>
2016-05-03 16:14:36 +0200

    Set default parameters for /schedule/channel API

--- lib/poms/api/uris.rb
+++ lib/poms/api/uris.rb
@@ -35,6 +35,8 @@ module Poms
         def uri_for_path(base_uri, path)
           base_uri.merge(path: "#{API_PATH}#{path}")
         end
+
+        private_class_method :uri_for_path
       end
 
       # URI for the /schedule API
@@ -43,13 +45,29 @@ module Poms
 
         module_function
 
-        def channel(base_uri, channel)
+        def channel(base_uri, channel, params = default_channel_params)
+          base_uri.query_values = params
           uri_for_path(base_uri, "/channel/#{channel}")
         end
 
         def uri_for_path(base_uri, path = nil)
           base_uri.merge(path: "#{API_PATH}#{path}")
         end
+
+        # To find the current broadcast we start looking at events from
+        # yesterday until now, sort descending and take the first one.
+        # It's probably not necessary to look as far back as yesterday, but we
+        # do need to know the most recent one.
+        def default_channel_params
+          {
+            max: 1,
+            sort: 'desc',
+            start: 1.day.ago.iso8601,
+            stop: Time.now.iso8601
+          }
+        end
+
+        private_class_method :default_channel_params, :uri_for_path
       end
     end
   end
